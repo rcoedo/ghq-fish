ce9c8ac9a4d480908163e33bee61ab56929acb5f
Roman Coedo <romancoedo@gmail.com>
2017-01-30 09:08:16 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ghq
+
+Ghq functions for the fish shell.
+
+## Install
+
+Install as a fisherman plugin
+
+```
+fisher rcoedo/ghq-fish
+```
+
+## Usage examples
+
+`__ghq_get rcoedo/ghq-fish` clones the github repository rcoedo/ghq-fish into $GHQ_ROOT/github.com/rcoedo/ghq-fish.
+`__ghq_get bb/rcoedo/ghq-fish` clones the bitbucket repository rcoedo/ghq-fish into $GHQ_ROOT/bitbucket.com/rcoedo/ghq-fish.
---
+++ functions/__ghq_get.fish
@@ -0,0 +1,15 @@
+function __ghq_get
+    if type -q ghq
+        if test (count $argv) = 1
+          if test (echo $argv[1] | grep -o "/" | wc -l | tr -d " ") = "2"
+                set -l __repository (echo $argv[1] | cut -d"/" -f 2-)
+                set -l __hub (echo $argv[1] | cut -d"/" -f 1)
+                if test $__hub = "bb"
+                      ghq get git@bitbucket.org:$__repository.git
+                end
+          else
+                ghq get -p $argv[1]
+          end
+       end
+   end
+end
