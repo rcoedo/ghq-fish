7081838dce15c08e5def40eb9a3d1d612b98136d
Roman Coedo <6409659+rcoedo@users.noreply.github.com>
2022-09-26 17:41:54 +0200

    update function to support identities

--- functions/__ghq_get.fish
+++ functions/__ghq_get.fish
@@ -1,18 +1,24 @@
 function __ghq_get
     if type -q ghq
-        if test (count $argv) = 1
-          if test (echo $argv[1] | grep -o "/" | wc -l | tr -d " ") = "2"
-                set -l __repository (echo $argv[1] | cut -d"/" -f 2-)
-                set -l __hub (echo $argv[1] | cut -d"/" -f 1)
-                if test $__hub = "bb"
-                      ghq get git@bitbucket.org:$__repository.git
-                end
-                if test $__hub = "gl"
-                      ghq get git@gitlab.com:$__repository.git
-                end
-          else
-                ghq get -p $argv[1]
-          end
-       end
+        argparse --name=__ghq_get 'u/user=' 'h/hub=' 'd/dry' -- $argv
+        or return
+
+        set -q _flag_user && set -l identity "-$_flag_user"
+        set -q _flag_hub || set -l _flag_hub "gh"
+        set -l hub (switch $_flag_hub
+                    case "gh"
+                        echo github.com
+                    case "bb"
+                        echo "bitbucket.org"
+                    case "gl"
+                        echo "gitlab.com"
+                  end)
+        set -l url "git@$hub$identity:$argv.git"
+
+        if set -q _flag_dry
+            echo $url
+        else
+            git clone $url $GHQ_ROOT/$hub/$argv
+        end
    end
 end
